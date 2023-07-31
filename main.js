f0c4b15425a577b6f3b398c637125e4b2e4bffee
Robbe Ceyssens <ceyssens.robbe@hotmail.com>
2023-07-31 20:19:43 +0200

    fix music en blink on movement

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 import './style.css'
 
 // Constants for the number of vertical areas and motion threshold
-const NUM_AREAS = 3;
+const NUM_AREAS = 4;
 const MOTION_THRESHOLD = 300;
 
 // Variables for the canvas, context, and video
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let video;
 
 // Variables for the canvas width, height, and area width
 let canvasWidth, canvasHeight, areaWidth;
@@ -16,24 +15,22 @@ let canvasWidth, canvasHeight, areaWidth;
 let prevFrameData, currFrameData;
 
 let cooldown = 0;
-// Variables for the audio elements
-let audioElements = [];
 
 // Array to keep track of whether there is motion in each area
 let motionDetected = new Array(NUM_AREAS).fill(false);
-
-for (let i = 0; i < motionDetected.length; i++) {
-  
-}
-const loops = [];
 
 function startCode(event){
 // Initialize the video stream
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(function (stream) {
-    
+    let audioElements = [];
+    for (let i = 0; i < NUM_AREAS; i++) {
+      const audio = new Audio(`AUDIO/wave_${i + 1}.mp3`);
+      audioElements.push(audio);
+    }
+
     // Set the video source to the stream
-    video = document.createElement('video');
+    let video = document.createElement('video');
     video.srcObject = stream;
     video.autoplay = true;
     video.playsinline = true;
@@ -51,14 +48,13 @@ navigator.mediaDevices.getUserMedia({ video: true })
       for (let i = 0; i < NUM_AREAS; i++) {
         prevFrameData[i] = new Uint8ClampedArray(canvasHeight * areaWidth * 4);
         currFrameData[i] = ctx.getImageData(i * areaWidth, 0, areaWidth, canvasHeight).data;
-
       }
 
       // Draw the video frames onto the canvas
       function draw() {
         // Draw the current video frame onto the canvas
         ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight);
-        
+
 
         // Loop through each vertical area
         for (let i = 0; i < NUM_AREAS; i++) {
@@ -67,53 +63,55 @@ navigator.mediaDevices.getUserMedia({ video: true })
 
           // Compare the current and previous frame data for the area
           let motion = 0;
-          for (let j = 0; j < currFrameData[i].length; j += 4) {
+          currFrameData[i].forEach((currFrame, j) => {
             const diff = Math.abs(currFrameData[i][j] - prevFrameData[i][j]) +
-              Math.abs(currFrameData[i][j + 1] - prevFrameData[i][j + 1]) +
-              Math.abs(currFrameData[i][j + 2] - prevFrameData[i][j + 2]);
+                Math.abs(currFrameData[i][j + 1] - prevFrameData[i][j + 1]) +
+                Math.abs(currFrameData[i][j + 2] - prevFrameData[i][j + 2]);
             if (diff > MOTION_THRESHOLD) {
               motion++;
             }
-          } if (motion > 0) {
+          });
+
+          if (motion > 0) {
             motionDetected[i] = true;
           }
-          for (let i = 0; i < motionDetected.length; i++) {
-            // 
-            if (motionDetected[i]) {
-              const audio = new Audio(`AUDIO/audio_${i+1}.mp3`);
-              audioElements.push(audio);
-              
-            }
-          }
-          
+
           // Output the area number where motion was detected
-          if (cooldown <= 0)
           if (motion > 0) {
             motionDetected[i] = true;
             cooldown = 100;
             console.log(`Motion detected in area ${i + 1}`);
-          
-  
-           } else {
+          } else {
             motionDetected[i] = false;
           }
-        else {
-          cooldown--;
-        }
-        for (let i = 0; i < audioElements.length; i++) {
-          if (motionDetected[i]) {
-            if (audioElements[i].paused) {
-              audioElements[i].play();
-              audioElements[i].loop = true;
+
+
+          audioElements.forEach((element, i) => {
+            if (motionDetected[i] && element.paused) {
+              switch (i) {
+                case 0:
+                  document.getElementById(`overlay4`).classList.add('blink');
+                  break;
+                case 1:
+                  document.getElementById(`overlay3`).classList.add('blink');
+                  break;
+                case 2:
+                  document.getElementById(`overlay2`).classList.add('blink');
+                  break;
+                case 3:
+                  document.getElementById(`overlay1`).classList.add('blink');
+                  break;
+              }
+              element.play();
+              element.loop = true;
+            } else if (!element.paused) {
+              setTimeout(function() {
+                document.getElementById(`overlay${i + 1}`).classList.remove('blink');
+                element.pause();
+                element.loop = false;
+              }, 2000);
             }
-          } else {
-            if (!audioElements[i].paused) {
-              audioElements[i].pause();
-              audioElements[i].loop = false;
-            }
-          }
-        }
-          
+          })
 
           // Update the previous frame data for the area
           prevFrameData[i].set(currFrameData[i]);
